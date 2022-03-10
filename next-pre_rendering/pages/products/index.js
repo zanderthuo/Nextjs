f521f6a5990e08929d0de3b85e41160f7aa72b16
zanderthuo <zanderthuo@gmail.com>
2022-03-10 09:43:02 +0300

    Finished static generation

---
+++ next-pre_rendering/pages/products/index.js
@@ -0,0 +1,31 @@
+function ProductList({ products }) {
+  return (
+    <>
+      <h1>List of Products</h1>
+      {products.map(product => {
+        return (
+          <div key={product.id}>
+            <h2>
+              {product.id} {product.title} {product.price}
+            </h2>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default ProductList
+
+export async function getStaticProps(){
+  console.log('Generating / Regenerating')
+  const response = await fetch('http://localhost:4000/products')
+  const data = await response.json()
+
+  return {
+    props: {
+      products: data,
+    },
+    revalidate: 30,
+  }
+}
